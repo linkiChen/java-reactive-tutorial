ba28e1509b573ddcc78b0828bd767d6b6137342b
s.chenjianfeng <s.chenjianfeng@byd.com>
2023-08-02 15:24:04 +0800

    rx连接池 及 事务配置

--- spring-data-r2dbc-sample/src/main/java/com/jacky/r2dbc/config/MysqlConnectionFactoryConfig.java
+++ spring-data-r2dbc-sample/src/main/java/com/jacky/r2dbc/config/MysqlConnectionFactoryConfig.java
@@ -2,11 +2,17 @@ package com.jacky.r2dbc.config;
 
 import dev.miku.r2dbc.mysql.MySqlConnectionConfiguration;
 import dev.miku.r2dbc.mysql.MySqlConnectionFactory;
+import io.r2dbc.pool.ConnectionPool;
+import io.r2dbc.pool.ConnectionPoolConfiguration;
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
+import org.springframework.transaction.ReactiveTransaction;
 import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.transaction.reactive.TransactionalOperator;
+
+import java.time.Duration;
 
 @Configuration
 
@@ -18,7 +24,7 @@ public class MysqlConnectionFactoryConfig {
      */
     @Bean
     public ConnectionFactory mysqlConnectionFactory() {
-        return MySqlConnectionFactory.from(
+        MySqlConnectionFactory connectionFactory = MySqlConnectionFactory.from(
                 MySqlConnectionConfiguration
                         .builder()
                         .host("10.167.150.29")
@@ -28,10 +34,25 @@ public class MysqlConnectionFactoryConfig {
                         .database("open_api")
                         .build()
         );
+        ConnectionPoolConfiguration poolConfiguration = ConnectionPoolConfiguration.builder(connectionFactory)
+                .maxIdleTime(Duration.ofMillis(2000))
+                .maxSize(10)
+                .initialSize(3)
+                .validationQuery("select 1")
+                .name("r2dbc-connection-pool")
+                .build();
+//        return connectionFactory;
+        return new ConnectionPool(poolConfiguration);
     }
 
     @Bean
     public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory) {
-        return new R2dbcTransactionManager(connectionFactory);
+        R2dbcTransactionManager txManager = new R2dbcTransactionManager(connectionFactory);
+        return txManager;
+    }
+
+    @Bean
+    public TransactionalOperator transactionalOperator(ReactiveTransactionManager txManager) {
+        return TransactionalOperator.create(txManager);
     }
 }
